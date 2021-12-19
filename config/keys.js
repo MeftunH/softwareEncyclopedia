ca7986ea5aa8da80f0ebfbddbcec697af9fb24f7
MeftunH <maftunhashimli@gmail.com>
2021-12-19 17:26:02 +0300

    First init and login register pages

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+export default {
+    firebaseConfig: {
+      apiKey: 'COPY_FROM_FIREBASE_SDK',
+      apiKey: "AIzaSyD8C5oTeGF1TKUP3Y7HichipSPfKoqwWt0",
+      authDomain: "softwareencyclopedia-e3a04.firebaseapp.com",
+      projectId: "softwareencyclopedia-e3a04",
+      storageBucket: "softwareencyclopedia-e3a04.appspot.com",
+      messagingSenderId: "672057606439",
+      appId: "1:672057606439:web:c4c9e91d4c4a4ef509a902"
+    }
+  }

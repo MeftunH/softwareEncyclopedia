0fbfb18f58c069af804a8274aef26adcacc21a24
MeftunH <maftunhashimli@gmail.com>
2021-12-20 01:22:45 +0300

    fetching data from firebase

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,99 +1,14 @@
-// import React, { useEffect, useState } from 'react'
-// import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
-// import styles from './styles';
-// import { firebase } from '../../firebase/config'
-
-// export default function HomeScreen(props) {
-
-//     const [entityText, setEntityText] = useState('')
-//     const [entities, setEntities] = useState([])
-
-//     const entityRef = firebase.firestore().collection('entities')
-//     const userID = props.extraData.id
-
-//     useEffect(() => {
-//         entityRef
-//             .where("authorID", "==", userID)
-//             .orderBy('createdAt', 'desc')
-//             .onSnapshot(
-//                 querySnapshot => {
-//                     const newEntities = []
-//                     querySnapshot.forEach(doc => {
-//                         const entity = doc.data()
-//                         entity.id = doc.id
-//                         newEntities.push(entity)
-//                     });
-//                     setEntities(newEntities)
-//                 },
-//                 error => {
-//                     console.log(error)
-//                 }
-//             )
-//     }, [])
-
-//     const onAddButtonPress = () => {
-//         if (entityText && entityText.length > 0) {
-//             const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-//             const data = {
-//                 text: entityText,
-//                 authorID: userID,
-//                 createdAt: timestamp,
-//             };
-//             entityRef
-//                 .add(data)
-//                 .then(_doc => {
-//                     setEntityText('')
-//                     Keyboard.dismiss()
-//                 })
-//                 .catch((error) => {
-//                     alert(error)
-//                 });
-//         }
-//     }
-
-//     const renderEntity = ({item, index}) => {
-//         return (
-//             <View style={styles.entityContainer}>
-//                 <Text style={styles.entityText}>
-//                     {index}. {item.text}
-//                 </Text>
-//             </View>
-//         )
-//     }
-
-//     return (
-//         <View style={styles.container}>
-//             <View style={styles.formContainer}>
-//                 <TextInput
-//                     style={styles.input}
-//                     placeholder='Add new entity'
-//                     placeholderTextColor="#aaaaaa"
-//                     onChangeText={(text) => setEntityText(text)}
-//                     value={entityText}
-//                     underlineColorAndroid="transparent"
-//                     autoCapitalize="none"
-//                 />
-//                 <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-//                     <Text style={styles.buttonText}>Add</Text>
-//                 </TouchableOpacity>
-//             </View>
-//             { entities && (
-//                 <View style={styles.listContainer}>
-//                     <FlatList
-//                         data={entities}
-//                         renderItem={renderEntity}
-//                         keyExtractor={(item) => item.id}
-//                         removeClippedSubviews={true}
-//                     />
-//                 </View>
-//             )}
-//         </View>
-//     )
-// }
 import { StyleSheet, Text, FlatList, View, SafeAreaView } from 'react-native';
 import AppBar from './appbar';
+import React, { useLayoutEffect,useState,useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../firebase/config'
 export default function HomeScreen({ navigation }) {
 
+    const [data,setData] = useState([]);
+    const [title,seTitle] = useState('');
+    const [description,setDescription] = useState([]);
+    
     var dataList = [
         { value: 'Algoritma yazılımın temelini oluşturur. Bir probleme yaklaşımınız, çözüme gidişiniz, kullandığınız yöntemler tasarladığınız algoritmaya göre yapılmaktadır. Algoritmayı hayatımızda farkında olmadan sürekli kullanmaktayız. Yaptığımız her hareket, her eylem aslında kendi içerisinde kendi algoritmasını barındırmaktadır. Demlediğimiz çayın, yaptığımız pilavın, hava karardığında odamızda yaktığımız ışığın… Her durumun aslında kendine has algoritması ve koşulları vardır. Tasarladığımız algoritmaları daha anlaşılır bir şekilde göstermek için akış diyagramları kullanırız', key: 'Algoritma', },
         { value: 'Yazdığımız kodlar da aslında günlük hayatta kullandığımız diller gibidir. Türkçede nasıl nokta, virgül, yüklem varsa programlama dillerinde de neye ne yapılacağını, hangi işaretler ile belirtileceğini tanımlamamız gerekmektedir. Her programlama dilinin de kendine göre bir sözdizimi vardır. Seçtiğimiz dilde kodlarımızı yazarken o dilin kurallarına uymamız gerekmektedir. Eğer kurallara uymazsanız zaten derleyici de yazdığınız kodun kullara uymadığını belirtir ve kodunuzu derlemez.', key: 'Söz dizimi (Syntax)', },
@@ -105,16 +20,32 @@ export default function HomeScreen({ navigation }) {
         { value: 'Büyük bir projenin içerisinde yer alıyorsanız, başka sistemlerle, servislerle iletişim halinde olma ihtiyacı duymuşsunuzdur. Yada kendiniz bir proje geliştiriyorsanız yapmak istediğiniz modül aslında başka biri tarafından yapılmış ve API olarak kullanıma açılmıştır.', key: 'API (Application Programming Interface)', },
         { value: 'Yazdığımız kodları derleyiciler neye göre derleyeceğini o dile ait framework’e bakarak anlarlar. C# uygulaması geliştiriyorsak bilgisayarımızda .net Framework kurulu olması gerekmektedir. Aynı şekilde bir Java uygulaması geliştiriyorsak bilgisayarımızda JDK olması gerekmektedir. Herhangi bir dilde yazılmış uygulamayı bilgisayarımızda çalıştırabilmek için de o uygulamanın framework’ü bilgisayarımızda kurulu olmalıdır', key: 'Framework', },
     ];
+    const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "concepts"));
+    const newConcepts = [];
+        querySnapshot.forEach((doc) => {
+            var title = doc.data().title;
+            var description = doc.data().description;
+            newConcepts.push({title: title, description: description});
+
+        console.log(doc.data());
+        });
+        setData(newConcepts);
+    }
+
+    useEffect(() => {
+        getData();
+      });
     return (
         <View>
             <AppBar></AppBar>
             <FlatList
-                data={dataList}
+                data={data}
                 style={styles.container}
                 renderItem={({ item }) =>
                     <View style={[styles.container, { flexDirection: 'column' },]}>
-                        <Text style={styles.textTitle}>{item.key}</Text>
-                        <Text style={styles.textContent}>{item.value}</Text>
+                        <Text style={styles.textTitle}>{item.title}</Text>
+                        <Text style={styles.textContent}>{item.description}</Text>
                     </View>}
             />
 

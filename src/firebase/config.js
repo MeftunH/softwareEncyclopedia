b2a0e8e2ddd811b90dd4d975ddc0497c386b977e
MeftunH <maftunhashimli@gmail.com>
2021-12-25 19:59:01 +0300

    changed firebase db

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,17 +4,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyD98gWfUrJcjVMlJNFHk9GUU8FJ4ys-kGA",
-
-  authDomain: "soft-58144.firebaseapp.com",
-
-  projectId: "soft-58144",
-
-  storageBucket: "soft-58144.appspot.com",
-
-  messagingSenderId: "544296432504",
-
-  appId: "1:544296432504:web:2275c39dfd3391b1aace8d"
+  apiKey: "AIzaSyAjUAd1oq0uLxvMvhYFXKnAxngFA4SF0H8",
+  authDomain: "mobileproject-ec098.firebaseapp.com",
+  projectId: "mobileproject-ec098",
+  storageBucket: "mobileproject-ec098.appspot.com",
+  messagingSenderId: "759396065454",
+  appId: "1:759396065454:web:1f7bbb9bfe257e7e1f0d3f"
 
 };
 

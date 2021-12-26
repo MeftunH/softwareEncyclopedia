3a1289e3aa3def7acdc8093096a59799dd27699e
MeftunH <maftunhashimli@gmail.com>
2021-12-26 16:41:04 +0300

    add alert to concept

--- src/Screens/DrawerScreens/AddConceptScreen.js
+++ src/Screens/DrawerScreens/AddConceptScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet, TextInput, Button, View, Text } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Button, View, Text,Alert } from "react-native";
 import { collection, addDoc, storage, getDocs } from "firebase/firestore";
 import { db, auth } from '../../firebase/config'
 import * as Notifications from 'expo-notifications';
@@ -18,15 +18,46 @@ export default function AddConcept() {
   const [text, onChangeText] = React.useState("Useless Text");
   const [number, onChangeNumber] = React.useState(null);
   const [addedUserEmail, setAddedUserEmail] = useState('');
+  const [errortext, setErrortext] = useState('');
+
   const user = auth.currentUser;
 
   const handleSave = async () => {
+    setErrortext('');
+
+    if (!title) {
+      alert('Please fill title');
+      return;
+    }
+    if (!description) {
+      alert('Please fill description');
+      return;
+    }
     setAddedUserEmail(user.email);
     const docRef = await addDoc(collection(db, "concepts"), {
       title: title,
       description: description,
       user_email: user.email,
-    });
+    }).then(() => {
+      Alert.alert(
+          "Eklendi",
+          "Basariyle Eklendi.",
+          [
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ],
+        );
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    Alert.alert(
+      errorCode,
+      errorMessage.replace("Firebase:" , ""),
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+  });;
 
 
     // PUSH NOTIFICATION
@@ -34,15 +65,13 @@ export default function AddConcept() {
 
     var expoPushTokens = [];
     await querySnapshot.forEach(async (doc) => {
-      console.log('push notification');
       expoPushTokens.push(doc.data().expoToken);
     });
-    console.log(expoPushTokens)
     const message = {
       to: expoPushTokens,
       sound: 'default',
       title: `${title} adinda yeni konseptimiz var.Goz atmak ister misin?`,
-      body: `${addedUserEmail} tarafindan ${title} adli yeni konsept eklendi`,
+      body: `${title} adli yeni konsept eklendi`,
       data: { someData: 'goes here' },
     };
 
